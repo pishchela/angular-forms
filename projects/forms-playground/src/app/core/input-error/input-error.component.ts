ace04ea2d722d8d4c90d998172768abfc45e7a01
pishchela <pishchela.aleksandr@gmail.com>
2023-10-08 14:21:51 -0400

    configurable messages

--- projects/forms-playground/src/app/core/input-error/input-error.component.ts
+++ projects/forms-playground/src/app/core/input-error/input-error.component.ts
@@ -1,16 +1,17 @@
 import { ChangeDetectionStrategy, Component, inject, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, KeyValue } from '@angular/common';
 import { ValidationErrors } from "@angular/forms";
 import { VALIDATION_ERROR_MESSAGES } from "./validation-error-messages.token";
+import { ErrorMessagePipe } from "../error-message.pipe";
 
 @Component({
   selector: 'app-input-error',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ErrorMessagePipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div *ngFor="let error of errors | keyvalue" class="input-error">
-      {{ errorsMap[error.key](error.value) }}
+    <div *ngFor="let error of errors | keyvalue; trackBy: trackByFn" class="input-error">
+      {{ error.key | errorMessage: error.value }}
     </div>
   `,
   styles: [
@@ -29,5 +30,9 @@ export class InputErrorComponent implements OnInit{
 
   ngOnInit() {
     console.warn(this.errorsMap);
+  }
+
+  trackByFn(index: number, item: KeyValue<string, any>) {
+    return item.key;
   }
 }
